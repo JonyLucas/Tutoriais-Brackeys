7374272b7ec5b9b0ea7aca0f2004032eba9ebc59
João Lucas <jony.lucas.br13@gmail.com>
2021-06-29 22:30:04 -0300

    Inicialização do projeto

---
+++ Assets/Generate Meshes/Scripts/GenerateMeshScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible to generate a quad mesh
+/// </summary>
+[RequireComponent(typeof(MeshFilter))]  
+public class GenerateMeshScript : MonoBehaviour {
+    
+    Mesh mesh;
+
+    Vector3[] vertices; // The vertices of the mesh to be draw
+    int[] triangles; // The index (on clockwise direction) of the vertices that will form a triangle
+
+    [SerializeField]
+    private float size; // The size of the mesh
+
+    [SerializeField]
+    private bool horizontal; // The direction of the mesh
+
+    [SerializeField]
+    private Vector3 origin; // The origin point
+
+    // Start is called before the first frame update
+    void Start() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateQuad(origin, size, horizontal);
+
+    }
+
+
+    private void CreateQuad(Vector3 origin, float size, bool horizontal) {
+
+        if (horizontal) {
+            vertices = new Vector3[] {
+                new Vector3(0,0,0),
+                new Vector3(size, 0, 0),
+                new Vector3(0, 0, size),
+                new Vector3(size, 0, size)
+            };
+
+            triangles = new int[] {
+                0, 2, 1,
+                3, 1, 2
+            };
+
+        } else {
+
+            vertices = new Vector3[] {
+                new Vector3(0,0,0),
+                new Vector3(0, size, 0),
+                new Vector3(size, 0, 0),
+                new Vector3(size, size, 0)
+            };
+
+            triangles = new int[] {
+                0, 1, 2,
+                3, 2, 1
+            };
+
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+    }
+
+
+}
---
+++ Assets/Generate Terrains/Scripts/TerrainGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+    public int xSize = 20, zSize = 20; // The dimensions of the terrain
+    public int maxHeight = 10;
+
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    Mesh mesh;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+        mesh = new Mesh ();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)]; // The number of vertices in each dimension is the number of edges + 1
+
+        int i = 0;
+        for (int z = 0; z <= zSize; z++) {
+            for (int x = 0; x <= xSize; x++) {
+                vertices[i++] = new Vector3 (x, Mathf.PerlinNoise(x * 0.6f, z * 0.1f) * maxHeight, z);
+            }
+        }
+
+        int nSquares = (xSize) * (zSize);
+        triangles = new int[nSquares * 6];
+
+
+        int vert = 0;
+        int triangle = 0;
+
+
+        for(int z = 0; z < zSize; z++) {
+            for (int x = 0; x < xSize; x++) {
+                triangles[triangle + 0] = vert;
+                triangles[triangle + 1] = vert + xSize + 1;
+                triangles[triangle + 2] = vert + 1;
+
+                triangles[triangle + 3] = vert + 1;
+                triangles[triangle + 4] = vert + xSize + 1;
+                triangles[triangle + 5] = vert + xSize + 2;
+
+
+                triangle += 6;
+                vert++;
+            }
+
+            vert++;
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+    }
+
+    private void OnDrawGizmos() {
+
+        if (vertices != null) {
+            int size = vertices.Length;
+
+            for (int i = 0; i < size; i++) {
+                Gizmos.DrawSphere (vertices[i], 0.2f);
+            }
+
+        }
+
+    }
+
+
+}
